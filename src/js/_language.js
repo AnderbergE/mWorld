5c42dd2dd071988a2c2eaa5c9781141a2dec129a
Erik Anderberg <Cloudiuz@gmail.com>
2014-05-23 11:10:15 +0200

    Language changing update
    Now with its very own function which will identify possible missing
    strings.

--- src/js/_language.js
+++ src/js/_language.js
@@ -1,17 +1,64 @@
 /**
  * This is the language variable for the game.
  * All text and speech files for the game should be here.
+ * Text strings are in LANG.TEXT.
+ * Speech sound files are in LANG.SPEECH.
+ * For the agent, use LANG.SPEECH.AGENT.
  *
- * It was developed with switching languages in mind. To do so:
- * replace LANG.TEXT and LANG.SPEECH with the new language.
- * (make sure that all properties exist in the new language file)
- * set LANG.SPEECH.AGENT according to the current user.agent.
+ * It was developed with switching languages in mind.
+ * To do so use function LANG.change.
+ * English is the default language and can be used as a
+ * template for other languages.
  *
  * NOTE: GLOBAL.FONT specifies which font that is used.
  */
 var LANG = {};
 
-LANG.TEXT = {
+LANG.change = function (text, speech) {
+	function replacer (orig, plus, missing, prefix) {
+		missing = missing || [];
+		if (!prefix) { prefix = ''; }
+		else { prefix += '/'; }
+
+		for (var p in orig) {
+			if (p === 'AGENT') { continue; } // AGENT is special case
+
+			if (!plus.hasOwnProperty(p)) {
+				missing.push(prefix + p);
+			} else if (typeof orig[p] !== 'string') {
+				replacer(orig[p], plus[p], missing, prefix + p);
+			} else {
+				orig[p] = plus[p];
+			}
+		}
+
+		return missing;
+	}
+
+	var m = replacer(LANG.TEXT, text);
+	if (m.length > 0) {
+		console.warn('TEXT is missing: ' + m);
+	}
+
+	m = replacer(LANG.SPEECH, speech);
+	if (m.length > 0) {
+		console.warn('SPEECH is missing: ' + m);
+	}
+
+	if (user && user.agent && user.agent.prototype.id) {
+		LANG.SPEECH.AGENT = LANG.SPEECH[user.agent.prototype.id];
+	} else {
+		LANG.SPEECH.AGENT = LANG.SPEECH.panda;
+	}
+};
+
+
+/*MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM*/
+/*                        English language (default)                         */
+/*WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW*/
+
+LANG.english = {};
+LANG.english.text = {
 	/* Entry state */
 	title: 'Magical Garden',
 	start: 'Start',
@@ -32,7 +79,7 @@ LANG.TEXT = {
 	pandaName: 'Panders'
 };
 
-LANG.SPEECH = {
+LANG.english.speech = {
 	birdhero: {
 		instruction1a: ['assets/audio/subgames/birdhero/instruction_1a.mp3', 'assets/audio/subgames/birdhero/instruction_1a.ogg'],
 		instruction1b: ['assets/audio/subgames/birdhero/instruction_1b.mp3', 'assets/audio/subgames/birdhero/instruction_1b.ogg'],
@@ -52,5 +99,6 @@ LANG.SPEECH = {
 	}
 };
 
-/* Use this for the agent speech, it makes it easier to switch between agents. */
+LANG.TEXT = LANG.english.text;
+LANG.SPEECH = LANG.english.speech;
 LANG.SPEECH.AGENT = LANG.SPEECH.panda;
