546933dde1c3ba488abfe8e3735420e2bf596026
Erik Anderberg <Cloudiuz@gmail.com>
2014-07-30 19:18:19 +0200

    DiceRepresentation now goes to 9

--- src/js/objects/Representations/DiceRepresentation.js
+++ src/js/objects/Representations/DiceRepresentation.js
@@ -4,7 +4,6 @@ DiceRepresentation.prototype.constructor = DiceRepresentation;
 /**
  * Dice representation of a number.
  * This is similar to DotsRepresentation, but has a pattern for the dot positions.
- * NOTE: Only available between numbers 1-6.
  * @param {number} number - The number to represent.
  * @param {number} x - X position.
  * @param {number} y - Y position.
@@ -14,13 +13,12 @@ DiceRepresentation.prototype.constructor = DiceRepresentation;
  */
 function DiceRepresentation (number, x, y, size, color) {
 	size = size || 100;
-	var radius = parseInt(size/8);
-	var top = radius+1;
-	var center = parseInt(size/2);
-	var bottom = size-radius-1;
+	this.radius = parseInt(size/8);
+	var top = this.radius+1;
+	var bottom = size-this.radius-1;
 	var left = top;
-	var middle = center;
 	var right = bottom;
+	var middle = parseInt(size/2);
 
 	/*
 	 * For more information about context:
@@ -29,63 +27,47 @@ function DiceRepresentation (number, x, y, size, color) {
 	var bmd = game.add.bitmapData(size, size);
 	var ctx = bmd.ctx;
 	ctx.fillStyle = color || '#000000';
+	ctx.beginPath();
+
 	if (number === 1) {
-		ctx.beginPath();
-		ctx.arc(middle, middle, radius, 0, Math.PI2);
-		ctx.closePath();
+		this.createDots(ctx, [[middle, middle]]);
 	} else if (number === 2) {
-		ctx.beginPath();
-		ctx.arc(left, top, radius, 0, Math.PI2);
-		ctx.moveTo(right, bottom);
-		ctx.arc(right, bottom, radius, 0, Math.PI2);
-		ctx.closePath();
+		this.createDots(ctx, [[left, top], [right, bottom]]);
 	} else if (number === 3) {
-		ctx.beginPath();
-		ctx.arc(left, top, radius, 0, Math.PI2);
-		ctx.moveTo(middle, middle);
-		ctx.arc(middle, middle, radius, 0, Math.PI2);
-		ctx.moveTo(right, bottom);
-		ctx.arc(right, bottom, radius, 0, Math.PI2);
-		ctx.closePath();
+		this.createDots(ctx, [[left, top], [middle, middle], [right, bottom]]);
 	} else if (number === 4) {
-		ctx.beginPath();
-		ctx.arc(left, top, radius, 0, Math.PI2);
-		ctx.moveTo(right, top);
-		ctx.arc(right, top, radius, 0, Math.PI2);
-		ctx.moveTo(left, bottom);
-		ctx.arc(left, bottom, radius, 0, Math.PI2);
-		ctx.moveTo(right, bottom);
-		ctx.arc(right, bottom, radius, 0, Math.PI2);
-		ctx.closePath();
+		this.createDots(ctx, [[left, top], [right, top], [left, bottom], [right, bottom]]);
 	} else if (number === 5) {
-		ctx.beginPath();
-		ctx.arc(left, top, radius, 0, Math.PI2);
-		ctx.moveTo(right, top);
-		ctx.arc(right, top, radius, 0, Math.PI2);
-		ctx.moveTo(middle, middle);
-		ctx.arc(middle, middle, radius, 0, Math.PI2);
-		ctx.moveTo(left, bottom);
-		ctx.arc(left, bottom, radius, 0, Math.PI2);
-		ctx.moveTo(right, bottom);
-		ctx.arc(right, bottom, radius, 0, Math.PI2);
-		ctx.closePath();
+		this.createDots(ctx, [[left, top], [right, top],
+			[middle, middle], [left, bottom], [right, bottom]]);
 	} else if (number === 6) {
-		ctx.beginPath();
-		ctx.arc(left, top, radius, 0, Math.PI2);
-		ctx.moveTo(right, top);
-		ctx.arc(right, top, radius, 0, Math.PI2);
-		ctx.moveTo(right, middle);
-		ctx.arc(right, middle, radius, 0, Math.PI2);
-		ctx.moveTo(left, middle);
-		ctx.arc(left, middle, radius, 0, Math.PI2);
-		ctx.moveTo(left, bottom);
-		ctx.arc(left, bottom, radius, 0, Math.PI2);
-		ctx.moveTo(right, bottom);
-		ctx.arc(right, bottom, radius, 0, Math.PI2);
-		ctx.closePath();
+		this.createDots(ctx, [[left, top], [right, top],
+			[left, middle], [right, middle], [left, bottom], [right, bottom]]);
+	} else if (number === 7) {
+		this.createDots(ctx, [[left, top], [right, top],
+			[left, middle], [middle, middle], [right, middle],
+			[left, bottom], [right, bottom]]);
+	} else if (number === 8) {
+		this.createDots(ctx, [[left, top],  [middle, top], [right, top],
+			[left, middle], [right, middle],
+			[left, bottom], [middle, bottom], [right, bottom]]);
+	} else if (number === 9) {
+		this.createDots(ctx, [[left, top],  [middle, top], [right, top],
+			[left, middle], [middle, middle], [right, middle],
+			[left, bottom], [middle, bottom], [right, bottom]]);
 	}
+
+	ctx.closePath();
 	ctx.fill();
 
 	Phaser.Sprite.call(this, game, x, y, bmd); // Parent constructor.
 	return this;
 }
+
+DiceRepresentation.prototype.createDots = function (ctx, dots) {
+	ctx.arc(dots[0][0], dots[0][1], this.radius, 0, Math.PI2);
+	for (var i = 1; i < dots.length; i++) {
+		ctx.moveTo(dots[i][0], dots[i][1]);
+		ctx.arc(dots[i][0], dots[i][1], this.radius, 0, Math.PI2);
+	}
+};
