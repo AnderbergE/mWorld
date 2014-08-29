cd21e4fdec377ac1edb8ab329219b28e652fd434
Erik Anderberg <Cloudiuz@gmail.com>
2014-08-29 10:09:18 +0200

    Bee: Refactored to new structure

--- src/js/states/subgames/numberpanelsubgames/BeeFlightGame.js
+++ src/js/states/subgames/numberpanelsubgames/BeeFlightGame.js
@@ -3,305 +3,260 @@
 /* Representations: All
 /* Range:           1--4
 /*WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW*/
-BeeFlightGame.prototype = Object.create(Subgame.prototype);
+BeeFlightGame.prototype = Object.create(NumberPanelSubgame.prototype);
 BeeFlightGame.prototype.constructor = BeeFlightGame;
 function BeeFlightGame () {
-	Subgame.call(this); // Call parent constructor.
+	NumberPanelSubgame.call(this); // Call parent constructor.
 }
+
+BeeFlightGame.prototype.pos = {
+	flowers: {
+		start: 300, stopOffset: -50
+	},
+	bee: {
+		x: 100, y: 300
+	},
+	agent: {
+		start: { x: -200, y: 700 },
+		stop: { x: 300, y: 500 },
+		scale: 0.25
+	}
+};
 
 /* Phaser state function */
 BeeFlightGame.prototype.preload = function () {
 	this.load.audio('beeMusic', ['assets/audio/subgames/beeflight/music.ogg', 'assets/audio/subgames/beeflight/music.mp3']);
+	this.load.audio('beePlaceholder', LANG.SPEECH.AGENT.hmm);
 
-	this.load.image('beeBg',     'assets/img/subgames/beeflight/bg.jpg');
+	this.load.image('beeBg',     'assets/img/subgames/beeflight/bg.png');
 	this.load.image('beeBody',   'assets/img/subgames/beeflight/humfrid.png');
 	this.load.image('beeFlower', 'assets/img/subgames/beeflight/flower.png');
 };
 
 /* Phaser state function */
 BeeFlightGame.prototype.create = function () {
-	var _this = this; // Subscriptions do not have access to 'this' object
-	var coords = {
-		flowers: {
-			start: 300, stop: this.world.width - 50
-		},
-		bee: {
-			x: 100, y: 300
-		},
-		agent: {
-			start: { x: -200, y: 700 },
-			stop: { x: 300, y: 500 },
-			scale: 0.25
-		}
-	};
-	coords.flowers.size = coords.flowers.stop - coords.flowers.start;
+	NumberPanelSubgame.prototype.create.call(this);
+
+	// This is used to know where the bee is.
+	this.atValue = 0;
 
 	// Add music
-	var music = this.add.audio('beeMusic', 1, true);
+	this.add.audio('beeMusic', 1, true).play();
 
-	// Add main game
+	// Add background
 	this.add.sprite(0, 0, 'beeBg', null, this.gameGroup);
-
-	// Agent is added to the game in the superclass, so set up correct start point.
-	this.agent.x = coords.agent.start.x;
-	this.agent.y = coords.agent.start.y;
-	this.agent.scale.set(coords.agent.scale);
-	this.agent.visible = true;
-	// Adding thought bubble that is used in the agent try mode.
-	this.agent.thought = this.add.group(this.gameGroup);
-	this.agent.thought.x = coords.agent.stop.x + 200;
-	this.agent.thought.y = coords.agent.stop.y - 200;
-	this.agent.thought.visible = false;
-	var thoughtBubble = this.add.sprite(0, 0, 'thought', null, this.agent.thought);
-	thoughtBubble.scale.x = -1;
-	thoughtBubble.anchor.set(0.5);
 	this.gameGroup.bringToTop(this.agent);
 
-	// Setup tree and its branches
-	var flowers = [];
-	var width = coords.flowers.size / this.amount;
+	// Setup flowers
+	var size = this.world.width - this.pos.flowers.stopOffset - this.pos.flowers.start;
+	var width = size / this.amount;
+	this.flowers = [];
 	for (var i = 0; i < this.amount; i++) {
-		flowers.push(this.add.sprite(coords.flowers.start + width*i, 400, 'beeFlower', null, this.gameGroup));
-		flowers[i].anchor.set(0.5, 0);
+		this.flowers.push(this.add.sprite(this.pos.flowers.start + width*i, 400, 'beeFlower', null, this.gameGroup));
+		this.flowers[i].anchor.set(0.5, 0);
 	}
 
 	// Setup bee
-	var bee = new BeeFlightBee(coords.bee.x, coords.bee.y);
-	this.gameGroup.add(bee);
-
-	// Add HUD
-	var buttons = new ButtonPanel(this.amount, this.representation, {
-		method: this.method,
-		x: coords.flowers.start,
-		y: 25,
-		size: coords.flowers.size,
-		onClick: pushNumber
-	});
-	buttons.visible = false;
-	this.hudGroup.add(buttons);
-	var yesnos = new ButtonPanel(2, GLOBAL.NUMBER_REPRESENTATION.yesno, {
-		x: coords.flowers.start,
-		y: 25,
-		size: coords.flowers.size,
-		onClick: pushYesno
-	});
-	yesnos.visible = false;
-	this.hudGroup.add(yesnos);
+	this.bee = new BeeFlightBee(this.pos.bee.x, this.pos.bee.y);
+	if (this.method === GLOBAL.METHOD.additionSubtraction) {
+		this.bee.addThought(-100, -75, this.representation[0]);
+	}
+	this.gameGroup.add(this.bee);
 
 
 	// Add Timeline/Tween functions
-	bee.moveTo = {
+	var _this = this; // Subscriptions do not have access to 'this' object
+	this.bee.moveTo = {
 		start: function () {
 			var t = new TimelineMax();
-			t.add(bee.move(coords.bee, 3));
-			t.add(bee.moveTurn(1));
+			t.add(_this.bee.move(_this.pos.bee, 3));
+			t.add(_this.bee.moveTurn(1));
 			return t;
 		},
 		flower: function (number) {
 			var t = new TimelineMax();
-			t.add(bee.move({ x: flowers[number].x }, 3));
-			t.add(bee.move({ y: flowers[number].y }, 1));
+			t.add(_this.bee.move({ x: _this.flowers[number].x }, 3));
+			t.add(_this.bee.move({ y: _this.flowers[number].y }, 1));
 			return t;
 		}
 	};
 	this.agent.moveTo = {
 		start: function () {
-			if (_this.agent.x === coords.agent.stop.x &&
-				_this.agent.y === coords.agent.stop.y) {
+			if (_this.agent.x === _this.pos.agent.stop.x &&
+				_this.agent.y === _this.pos.agent.stop.y) {
 				return new TweenMax(_this.agent);
 			}
-			return _this.agent.move({ x: coords.agent.stop.x, y: coords.agent.stop.y }, 3);
+			return _this.agent.move({ x: _this.pos.agent.stop.x, y: _this.pos.agent.stop.y }, 3);
 		}
 	};
-
-
-	/* Function to trigger when a number button is pushed */
-	function pushNumber (number) {
-		_this.disable(true);
-
-		var current = _this.currentNumber-1;
-		var result = _this.tryNumber(number);
-
-		var t = new TimelineMax();
-		t.add(bee.moveTo.flower(number-1));
-		if (!result) { // Correct :)
-			t.add(new TweenMax(flowers[current], 1, { tint: '0xffffff' }));
-		} else { // Incorrect :(
-			t.add(new TweenMax(flowers[current], 1, { tint: '0xff33ff' }));
-		}
-		t.add(bee.moveTo.start());
-		t.addCallback(function () { _this.nextRound(); });
-	}
-	/* Function to trigger when a yes/no button is pushed */
-	function pushYesno (value) {
-		if (!value) {
-			showNumbers();
-		}
-		else { pushNumber(_this.agent.lastGuess); }
-	}
-
-	/* Show the number panel, hide the yes/no panel and enable input */
-	function showNumbers () {
-		_this.disable(true);
-		buttons.reset();
-		fade(yesnos, false);
-		fade(buttons, true).eventCallback('onComplete', _this.disable, false, _this);
-
-		if (_this.agent.visible) { _this.agent.eyesFollowPointer(); }
-	}
-	/* Show the yes/no panel, hide the number panel and enable input */
-	function showYesnos () {
-		_this.disable(true);
-		yesnos.reset();
-		fade(buttons, false);
-		fade(yesnos, true).eventCallback('onComplete', _this.disable, false, _this);
-
-		if (_this.agent.visible) { _this.agent.eyesFollowPointer(); }
-	}
-	/* Hide the number and yes/no panel */
-	function hideButtons () {
-		_this.disable(true);
-		fade(buttons, false);
-		fade(yesnos, false);
-
-		if (_this.agent.visible) { _this.agent.eyesStopFollow(); }
-	}
-
-	function newFlower () {
-		var t = new TimelineMax();
-		t.add(new TweenMax(flowers[_this.currentNumber-1], 1, { tint: '0x33ffff' }));
-		return t;
-	}
-
-	/* Have the agent guess a number */
-	function agentGuess () {
-		_this.agent.guessNumber(_this.currentNumber, 1, _this.amount);
-
-		return TweenMax.fromTo(_this.agent.thought.scale, 1.5,
-			{ x: 0, y: 0 },
-			{ x: 1, y: 1,
-				ease: Elastic.easeOut,
-				onStart: function () {
-					_this.agent.thought.visible = true;
-					if (_this.agent.thought.guess) { _this.agent.thought.guess.destroy(); }
-					// say('birdheroAgentHmm', _this.agent).play();
-				},
-				onComplete: function () {
-					_this.agent.thought.guess = new NumberButton(_this.agent.lastGuess, _this.representation, {
-						x: -20, y: -30, disabled: true
-					});
-					_this.agent.thought.add(_this.agent.thought.guess);
-					// TODO: Agent should say something here based on how sure it is.
-					showYesnos();
-				}
-			});
-	}
-
-/*
-	function pointAtBole (number) {
-		var first = tree.pieces[0];
-		var center = { x: first.width*tree.scale.x/2, y: first.height*tree.scale.y/2*1.3 }; // 1.2 is a offset
-		var start = first.world;
-		var offset = 70;
-		var arrow = game.add.sprite(start.x + center.x + offset, start.y - center.y, 'lizardArrow', null, this.gameGroup);
-		arrow.visible = false;
-
-		var t = new TimelineMax();
-		t.addCallback(function () { arrow.visible = true; });
-		for (var i = 0; i < number; i++) {
-			var pos = tree.pieces[i].world;
-			if (i !== 0) { t.add(new TweenMax(arrow, 1, { x: '+=' + offset, y: pos.y - center.y })); }
-			t.add(new TweenMax(arrow, 1, { x: '-=' + offset }));
-		}
-		t.addCallback(function () { arrow.destroy(); }, '+=0.5');
-		return t;
-	}
-*/
-
-	function instructionIntro () {
-		var t = new TimelineMax();
-		//t.addSound(speech, bird, 'instruction1a');
-		// t.add(pointAtBole(_this.currentNumber));
-		t.add(fade(buttons, true));
-		t.add(buttons.highlight(1));
-		//t.addSound(speech, bird, 'instruction1b');
-		return t;
-	}
-
-	this.modeIntro = function () {
-		// Most of the intro is in modePlayerDo. But we need a slight wait
-		// so that the game world can set up.
-		var t = new TimelineMax();
-		// t.addSound('lizardPlaceholder', lizard);
-		t.addCallback(function () { _this.nextRound(); });
-	};
-
-	this.modePlayerDo = function (intro, tries) {
-		if (tries > 0) {
-			showNumbers();
-		} else { // if intro or first try
-			var t = new TimelineMax();
-			if (intro) {
-				t.skippable();
-				t.add(newFlower());
-				t.add(instructionIntro());
-			} else {
-				t.add(newFlower());
-			}
-			t.addCallback(showNumbers);
-		}
-	};
-
-	this.modePlayerShow = function (intro, tries) {
-		if (tries > 0) {
-			showNumbers();
-		} else { // if intro or first try
-			var t = new TimelineMax();
-			if (intro) {
-				t.skippable();
-				t.eventCallback('onStart', function () { hideButtons(); });
-				t.add(_this.agent.moveTo.start());
-				t.addLabel('agentIntro');
-				//t.addSound('birdheroAgentShow', _this.agent);
-				t.add(_this.agent.wave(3, 1), 'agentIntro');
-				//t.eventCallback('onComplete', function () { _this.sound.removeByKey('birdheroAgentShow'); });
-			}
-			t.add(newFlower());
-			t.addCallback(showNumbers);
-		}
-	};
-
-	this.modeAgentTry = function (intro, tries) {
-		var t = new TimelineMax();
-		if (tries > 0) {
-			_this.agent.eyesStopFollow();
-			// TODO: Add more specified sounds?
-			//t.addSound('birdheroAgentOops', _this.agent);
-			t.add(agentGuess());
-		} else { // if intro or first try
-			if (intro) {
-				t.skippable();
-				t.eventCallback('onStart', function () { hideButtons(); });
-				t.add(_this.agent.moveTo.start()); // Agent should be here already.
-				//t.addSound('birdheroAgentTry', _this.agent);
-				//t.eventCallback('onComplete', function () { _this.sound.removeByKey('birdheroAgentTry'); });
-			}
-			t.add(newFlower());
-			t.add(agentGuess());
-		}
-	};
-
-
-	/* Play music on the first mode. */
-	function playMusic () {
-		music.play();
-		EventSystem.unsubscribe(GLOBAL.EVENT.modeChange, playMusic);
-	}
-	EventSystem.subscribe(GLOBAL.EVENT.modeChange, playMusic);
 
 
 	// Everything is set up! Blast off!
 	this.startGame();
+};
+
+BeeFlightGame.prototype.getOptions = function () {
+	var size = this.world.width - this.pos.flowers.stopOffset - this.pos.flowers.start;
+	return {
+		buttons: {
+			x: this.pos.flowers.start,
+			y: 25,
+			size: size,
+		},
+		yesnos: {
+			x: this.pos.flowers.start,
+			y: 25,
+			size: size,
+		}
+	};
+};
+
+BeeFlightGame.prototype.instructionIntro = function () {
+	var t = new TimelineMax();
+	//t.addSound(speech, bird, 'instruction1a');
+	// t.add(pointAtBole(_this.currentNumber));
+	t.add(fade(this.buttons, true));
+	t.add(this.buttons.highlight(1));
+	//t.addSound(speech, bird, 'instruction1b');
+	return t;
+};
+
+BeeFlightGame.prototype.newFlower = function () {
+	var t = new TimelineMax();
+	t.add(new TweenMax(this.flowers[this.currentNumber-1], 1, { tint: '0x33ffff' }));
+	return t;
+};
+
+BeeFlightGame.prototype.startStop = function () {
+	// Do nothing
+};
+
+BeeFlightGame.prototype.startBelow = function (t) {
+	t.add(this.runNumber(this.rnd.integerInRange(1, this.currentNumber - 1), true));
+};
+
+BeeFlightGame.prototype.startAbove = function (t) {
+	t.add(this.runNumber(this.rnd.integerInRange(this.currentNumber + 1, this.amount), true));
+};
+
+BeeFlightGame.prototype.startThink = function (t) {
+	t.addCallback(function () {
+		this.addToNumber = this.rnd.integerInRange(1, this.amount);
+		this.bee.thought.guess.number = this.addToNumber;
+		this.updateButtons();
+	}, null, null, this);
+	t.add(this.bee.think());
+};
+
+BeeFlightGame.prototype.runNumber = function (number) {
+	this.disable(true);
+
+	var current = this.currentNumber-1;
+	var result = this.tryNumber(number);
+
+	var t = new TimelineMax();
+	t.add(this.bee.moveTo.flower(number-1));
+	if (!result) { // Correct :)
+		t.addCallback(this.hideButtons, null, null, this);
+		t.add(new TweenMax(this.flowers[current], 1, { tint: '0xffffff' }));
+		t.add(this.bee.moveTo.start());
+	} else { // Incorrect :(
+		t.add(new TweenMax(this.flowers[current], 1, { tint: '0xff33ff' }));
+		this.doReturnFunction(t, number, result);
+	}
+
+	if (this.isRelative) {
+		t.addCallback(function () {
+			this.addToNumber = parseInt(this.atValue);
+			this.updateButtons();
+		}, null, null, this);
+	}
+
+	return t;
+};
+
+BeeFlightGame.prototype.returnToStart = function (t) {
+	this.atValue = 0;
+	t.add(this.bee.moveTo.start());
+};
+
+BeeFlightGame.prototype.returnNone = function (t, number) {
+	this.atValue = number;
+	// Do nothing else
+};
+
+BeeFlightGame.prototype.returnToPreviousIfHigher = function (t, number, diff) {
+	if (diff > 0) {
+		t.addSound('beePlaceholder'); // I think that is too high. Mouth is open, no animation.
+	} else {
+		this.returnNone(t, number);
+	}
+};
+
+BeeFlightGame.prototype.returnToPreviousIfLower = function (t, number, diff) {
+	if (diff < 0) {
+		t.addSound('beePlaceholder'); // I think that is too low. Mouth is open, no animation.
+	} else {
+		this.returnNone(t, number);
+	}
+};
+
+
+/*MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM*/
+/*                 Overshadowing Subgame mode functions                      */
+/*WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW*/
+BeeFlightGame.prototype.modeIntro = function () {
+	var t = new TimelineMax();
+	t.addCallback(this.nextRound, null, null, this);
+};
+
+BeeFlightGame.prototype.modePlayerDo = function (intro, tries) {
+	if (tries > 0) {
+		this.showNumbers();
+	} else { // if intro or first try
+		var t = new TimelineMax();
+		if (intro) {
+			t.skippable();
+			t.add(this.newFlower());
+			t.add(this.instructionIntro());
+		} else {
+			t.add(this.newFlower());
+		}
+		t.addCallback(this.showNumbers, null, null, this);
+	}
+};
+
+BeeFlightGame.prototype.modePlayerShow = function (intro, tries) {
+	if (tries > 0) {
+		this.showNumbers();
+	} else { // if intro or first try
+		var t = new TimelineMax();
+		if (intro) {
+			t.skippable();
+			t.add(this.agent.moveTo.start());
+			t.addLabel('agentIntro');
+			t.add(this.agent.wave(3, 1), 'agentIntro');
+		}
+		t.add(this.newFlower());
+		t.addCallback(this.showNumbers, null, null, this);
+	}
+};
+
+BeeFlightGame.prototype.modeAgentTry = function (intro, tries) {
+	var t = new TimelineMax();
+	if (tries > 0) {
+		this.agent.eyesStopFollow();
+	} else { // if intro or first try
+		if (intro) {
+			t.skippable();
+			t.add(this.agent.moveTo.start()); // Agent should be here already.
+		}
+		t.add(this.newFlower());
+		
+	}
+
+	t.add(this.agentGuess());
+	t.addCallback(this.showYesnos, null, null, this);
 };
 
 
