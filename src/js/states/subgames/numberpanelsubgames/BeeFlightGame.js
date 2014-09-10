b854eb79701ea924c0027f69c34e937d14ef43f8
Erik Anderberg <Cloudiuz@gmail.com>
2014-09-10 13:08:37 +0200

    Bee: Graphics update and animations

--- src/js/states/subgames/numberpanelsubgames/BeeFlightGame.js
+++ src/js/states/subgames/numberpanelsubgames/BeeFlightGame.js
@@ -144,8 +144,8 @@ BeeFlightGame.prototype.newFlower = function () {
 	return t;
 };
 
-BeeFlightGame.prototype.startStop = function () {
-	// Do nothing
+BeeFlightGame.prototype.startStop = function (t) {
+	t.addSound('beePlaceholder', this.bee);
 };
 
 BeeFlightGame.prototype.startBelow = function (t) {
@@ -290,6 +290,28 @@ function BeeFlightBee (x, y) {
 	this.x = x || 0;
 	this.y = y || 0;
 
-	this.body = game.add.sprite(0, 0, 'bee', 'humfrid', this);
+	this.body = this.create(0, 0, 'bee', 'body');
 	this.body.anchor.set(0.5);
+	this.mouth = this.create(50, 35, 'bee', 'mouth_happy');
+	this.mouth.anchor.set(0.5);
+	this.wings = this.create(-25, -43, 'bee', 'wings0');
+	this.wings.anchor.set(0.5);
+
+	this.talk = TweenMax.to(this.mouth, 0.2, {
+		frame: this.mouth.frame+1, roundProps: 'frame', ease: Power0.easeInOut, repeat: -1, yoyo: true, paused: true
+	});
+
+	this._flap = TweenMax.to(this.wings, 0.1, {
+		frame: this.wings.frame+1, roundProps: 'frame', ease: Power0.easeInOut, repeat: -1, yoyo: true, paused: true
+	});
+
+	this.flap(true);
 }
+
+BeeFlightBee.prototype.flap = function (on) {
+	if (on && this._flap.paused()) {
+		this._flap.restart(0);
+	} else {
+		this._flap.pause(0);
+	}
+};
