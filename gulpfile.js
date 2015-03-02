8eb11794cc3fa33af9c9b60bd32070a47d33cf98
Erik Anderberg <Cloudiuz@gmail.com>
2015-03-02 21:23:43 +0100

    Production gulp task added
    Also made sure that images are only optimized in production mode.

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,10 @@ var autoprefixer = require('gulp-autoprefixer');
 // Asset requires
 var imagemin = require('gulp-imagemin');
 
+// Production requires
+var rename = require('gulp-rename');
+var delet = require('del');
+var replace = require('gulp-replace');
 
 // Constants
 var SRC = 'src/';
@@ -27,37 +31,45 @@ var SCRIPT = SRC + 'script/';
 var STYLE = SRC + 'style/';
 var ALL = '**/*';
 var DEST = 'build/';
+var PRODUCTION = 'production/';
+
+// Build related variables, are setup depending on build type.
+var destination = DEST;
+var optimize = false;
 
 var getBundleName = function () {
-	var version = require('./package.json').version;
-	var name = require('./package.json').name;
-	return name.toLowerCase() + '.' + version + '.min';
+	// var version = require('./package.json').version;
+	// var name = require('./package.json').name;
+	return 'game';
 };
 
 
 // Tasks
-gulp.task('assets', function() {
-	return gulp.src([
+gulp.task('assets', function () {
+	var value = gulp.src([
 			AUDIO + ALL + '.*',
 			'!' + IMAGE + '**/separated/*.*', // Skip the separated files
 			IMAGE + ALL + '.*',
 			SRC + '*.js'
-		], { base: SRC })
-		.pipe(imagemin({ optimizationLevel: 2 })) // Minimize image sizes
-		.pipe(gulp.dest(DEST));
+		], { base: SRC });
+
+	if (optimize) {
+		value = value.pipe(imagemin({ optimizationLevel: 2 })); // Minimize image sizes
+	}
+
+	return value.pipe(gulp.dest(destination));		
 });
 
-gulp.task('html', function() {
+gulp.task('html', function () {
 	return gulp.src(SRC + ALL + '.html')
-		.pipe(gulp.dest(DEST));
+		.pipe(gulp.dest(destination));
 });
 
-gulp.task('browserify', function() {
+gulp.task('browserify', function () {
 	var bundler = browserify('./' + SCRIPT + 'game.js', { debug: true });
 
 	var bundle = function() {
-		return bundler
-			.bundle()
+		return bundler.bundle()
 			.on('error', function (err) {
 				util.beep();
 				util.log(util.colors.red('Error: '), err.message);
@@ -66,10 +78,8 @@ gulp.task('browserify', function() {
 			.pipe(source(getBundleName() + '.js'))
 			.pipe(buffer())
 			.pipe(sourcemaps.init({ loadMaps: true }))
-			// Add transformation tasks to the pipeline here.
-			//.pipe(uglify())
 			.pipe(sourcemaps.write('./'))
-			.pipe(gulp.dest(DEST));
+			.pipe(gulp.dest(destination));
 	};
 
 	return bundle();
@@ -94,7 +104,7 @@ gulp.task('styles', function() {
 			this.emit('end');
 		})
 		.pipe(autoprefixer('last 2 version'))
-		.pipe(gulp.dest(DEST));
+		.pipe(gulp.dest(destination));
 });
 
 gulp.task('watch', function() {
@@ -114,11 +124,47 @@ gulp.task('watch', function() {
 // Start server at: http://localhost:9000/index.html
 gulp.task('connect', function() {
 	connect.server({
-		root: DEST,
+		root: destination,
 		port: 9000
 	});
 });
 
 gulp.task('default', ['assets', 'html', 'scripts', 'styles', 'watch', 'connect'], function () {
 	util.log(util.colors.green('All tasks have started!'));
+});
+
+
+// Production related.
+gulp.task('setup-vars', function () {
+	destination = PRODUCTION;
+	optimize = true;
+});
+
+// Production related.
+gulp.task('railsify-script', ['scripts'], function () {
+	gulp.src(PRODUCTION + getBundleName() + '.js')
+		.pipe(replace(/\'(?:img|audio)\/([^\']*)/gi, '\'<%= asset_path(\'$1\') %>'))
+		.pipe(rename(function (path) { path.extname = ".js.erb"; }))
+		.pipe(gulp.dest(PRODUCTION));
+});
+
+// Production related.
+// When generating files designed to be used on the server, run this:
+gulp.task('production', ['setup-vars', 'assets', 'html', 'styles', 'railsify-script'], function () {
+	// Cleanup of old js files.
+	delet([
+		PRODUCTION + getBundleName() + '.js',
+		PRODUCTION + getBundleName() + '.js.map'
+	]);
+
+	// Instructions for how to add the files in the server codebase.
+	util.log(
+		util.colors.green('\nYour files will be in ./production/\n') +
+		'Copy audio/ to app/assets/audio/\n' +
+		'Copy img/ to app/assets/images/\n' +
+		'Copy ' + getBundleName() + '.js.erb to app/assets/javascripts/\n' +
+		'Copy phaser.js and TweenMax.js to lib/assets/javascripts/\n\n' +
+		'Do NOT copy html or css, if necessary, manually update:\n' +
+		'app/views/application/index.html.erb or app/assets/stylesheets/game.css.scss\n'
+	);
 });
